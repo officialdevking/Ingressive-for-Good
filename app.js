15b1ef624d01a914c6565335a080f191da95bb98
officialdevking <kingsleysunday998@gmail.com>
2021-09-10 10:39:14 -0700

    Second Commit

---
+++ app.js
@@ -0,0 +1,6 @@
+document.getElementById('hamburger').onclick = ()=>{
+    document.getElementById('sliding_menu').style.display = "block"
+}
+document.getElementById('cancel').onclick = ()=>{
+    document.getElementById('sliding_menu').style.display = ""
+}
