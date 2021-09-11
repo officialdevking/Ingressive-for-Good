fdd62b8047ab87473cf8b3ebfafa33c495c5503b
officialdevking <kingsleysunday998@gmail.com>
2021-09-11 04:48:48 -0700

    last

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 document.getElementById('hamburger').onclick = ()=>{
     document.getElementById('sliding_menu').style.display = "block"
+    document.querySelector('body').classList.add("slide_open")
 }
 document.getElementById('cancel').onclick = ()=>{
     document.getElementById('sliding_menu').style.display = ""
+    document.querySelector('body').classList.remove("slide_open")
+
 }
